ce63e356f34ff38c26975e2a8e955d86bf01f91d
George Boshkaev <ghost00763@users.noreply.github.com>
2026-05-06 18:25:46 +0300

    Add files via upload

--- scripts_3.py
+++ scripts_3.py
@@ -14,11 +14,9 @@ image = red
 coordinates = (left_crop, 0,image.width-ride_crop, image.height)
 red2 = image.crop(coordinates)
 
-
 image1 = red1
 image2 = red2
 red3 = Image.blend(image1, image2, 0.5)
-
 
 ride_crop = 50
 image = blue
@@ -35,7 +33,6 @@ image1 = blue1
 image2 = blue2
 blue3 = Image.blend(image1, image2,0.5)
 
-
 image = green
 coordinates = (25,0, image.width-25,image.height)
 green3 = image.crop(coordinates)
@@ -43,7 +40,7 @@ green3 = image.crop(coordinates)
 r = red3
 b= blue3
 g= green3
-new_image = Image.merge("RGB",(r,b,g))
+new_image = Image.merge("RGB",(r,g,b))
 new_image.save("Monro_merge.jpg")
 
 image = Image.open("Monro_merge.jpg")
